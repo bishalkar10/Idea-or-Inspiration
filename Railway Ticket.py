ce7660db7fb997d9540623be7681cd56b5d7bc2d
bishalkar10 <112653391+bishalkar10@users.noreply.github.com>
2022-10-09 10:55:57 +0530

    Update Railway Ticket.py
    1. cancel_ticket () function is successfully implemented. 
    2. self.seat_no is now an instance variable.

--- Railway Ticket.py
+++ Railway Ticket.py
@@ -1,13 +1,14 @@
 class Railway :
     train_Name = 'Howrah-Chakradharpur'
-    # list of all seats. this list is sorted so we can append canceled tickets seat no. 
-    seat = sorted([x for x in range (1,1001)])
+    # list of all seats.
+    seat = [x for x in range (1,1001)]
     
     def __init__ (self, name, b_station, d_station) :
         self.name = name            # Passenger Name
         self.b_station = b_station  # Boarding Station Name
         self.d_station = d_station  # Destination Station Name
-        
+        self.seat_no = 0
+
     def info(self):
         print (f'Passenger Name - {self.name}.')
         print (f'Boarding Station  - {self.b_station}.')
@@ -15,10 +16,10 @@ class Railway :
         print (f'Train Name - {self.train_Name}.')
     
     def book_seat(self):           # This is a Seat Booking Function
-        if len(self.seat) >= 1 :
-            seat_no = self.seat[0]  # I am unable to access this 'Seat_no variable from anywhere else.'
-            del self.seat[0]
-            print (f'Your Seat No. is {seat_no}.')
+        if len(Railway.seat) >= 1 :
+            self.seat_no = Railway.seat[0]
+            del Railway.seat[0]
+            print (f'Your Seat No. is {self.seat_no}.')
             print ()
         else :
             print ('Sorry! All seats are booked.')
@@ -28,16 +29,17 @@ class Railway :
         self.info()
         self.book_seat()
        
-    def cancel_ticket(self):      # I am trying to Implement Ticket Canceling Function
-        self.seat.append (self.seat_no)
-
+    def cancel_ticket(self):
+        Railway.seat.append (self.seat_no)
+        Railway.seat = sorted (Railway.seat)
+        print ('Your Ticket has been cancelled.')
+        print (f'Seat No. {self.seat_no} is now available to book.')
+        
 p1 = Railway('Bishal Kar', 'Kolkata', 'Bankura')
 p2 = Railway('Rahul Halder', 'Purulia', 'Bankura')
 p1.book_ticket()
 p2.book_ticket()
 
-# I am trying to implement ticket Canceling Function.But not not able to implement it.
-# p2.cancel_ticket()
+p2.cancel_ticket()
 
-# print (Railway.seat)
-       
+ 
